f3f022dddd56f53d27a5cff74bd5c76a34b6cef4
pranay2492 <pranaysharma2492@gmail.com>
2018-01-17 23:18:55 +0530

    1t

---
+++ src/test/java/mavenPro/Testing.java
@@ -0,0 +1,32 @@
+package mavenPro;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Testing {
+
+	public void show()
+	{
+	//System.out.println("Git done");
+	}
+	@Test
+	public void pass()
+	{
+		int a,b;
+		a=1;b=2;
+		assertTrue(1+2 == 3);
+		
+	}
+	
+	@Test
+	public void fail()
+	{
+		int a,b;
+		a=3;b=1;
+		assertTrue(a + b == 4);
+		
+	}
+
+	
+}
